dc53df12751e83aae8e8df39297ffb249ba757c5
CLAUDISNEI <claudisneibello@gmail.com>
2021-08-07 00:53:19 -0300

    Finalizado a configuração básica do projeto

---
+++ database/database.js
@@ -0,0 +1,13 @@
+//importando o sequelize
+const Sequelize = require('sequelize');
+
+/**Criando a connection com os parâmetros: nome do banco de dados,
+ * nome do usuário, senha do usuário, e um json com o host e o tipo
+ * de banco de dados
+ */
+const connection = new Sequelize('guiapress','root','123456',{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -3,10 +3,31 @@ const express = require('express');
 //criando instância
 const app = express();
 
+/**BANCO DE DADOS *************************/
+//importando a conexão com o banco de dados
+const connection = require('./database/database');
+
+//realizando a autenticação com o banco de dados
+connection
+    .authenticate()
+    .then(()=>{
+        console.log("Conexão realizada com sucesso!");
+    }).catch((error)=>{
+        console.log(error);
+    });
+/***************************************** */
+/***************************************** */
+//configurando a view engine
+app.set('view engine', 'ejs');
+
+//define que o aplicativo usará arquivos estaticos, informando 
+//em que pasta eles se estão localizados. Deve-se criar a pasta
+app.use(express.static('public'));
+ 
 
 //criando rota principal
 app.get('/',(req,res)=>{
-    res.send("Bem vindo ao meu site");
+    res.render('index');
 });
 
 //iniciando a aplicação
